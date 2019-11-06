9575c64d9dff16500066ced21202bedd134acb19
david <davidkuhn@stud.unibas.ch>
2019-11-06 14:06:22 +0100

    added corridor inspector

---
+++ src/app/components/exhibitions/inspectors/corridor/corridor-inspector.component.ts
@@ -0,0 +1,36 @@
+import {Component, Input} from '@angular/core';
+import {Corridor} from '../../../../model/implementations/corridor.model';
+import {Textures} from '../../../../model/interfaces/general/textures.model';
+
+@Component({
+    selector: 'app-corridor-inspector',
+    templateUrl: 'corridor-inspector.component.html',
+    styleUrls: ['corridor-inspector.component.scss']
+})
+export class CorridorInspectorComponent {
+    @Input('corridor')
+    private _corridor: Corridor = null;
+
+    /** List of available textures. */
+    private _textures: string[] = Textures.map(v => v.toString());
+
+    /**
+     * Default constructor.
+     */
+    constructor() {}
+
+    /**
+     *
+     */
+    get textures() {
+        return this._textures;
+    }
+
+
+    /**
+     * Getter for the
+     */
+    get corridor(): Corridor {
+        return this._corridor;
+    }
+}
